6f9cad29ef1fec94f09978e14b56edb0a3427ee3
Martin olsen <mbradyolsen@gmail.com>
2024-12-18 22:40:19 +0100

    gr

---
+++ js/javascript.js
@@ -0,0 +1,44 @@
+function cabin(choice) {
+    if(choice ==1){
+       document.getElementById("p1").style.display = "none";
+
+       document.getElementById("cabin").style.display = "none";
+        document.getElementById("game").style.display = "block";
+    
+    }
+    }
+    function monster(choice) {
+        if(choice ==1){
+            document.getElementById("monster").style.display = "none";
+            document.getElementById("game").style.display = "block";
+        
+        }
+        }
+        
+        
+        function cabin(choice) {
+            if(choice ==1){
+                document.getElementById("cabin").style.display = "none";
+                document.getElementById("game").style.display = "block";
+            
+            }
+            }
+
+           
+           
+            function cabin(choice) {
+                if(choice ==1){
+                    document.getElementById("cabin").style.display = "none";
+                    document.getElementById("game").style.display = "block";
+                
+                }
+                }
+                
+                
+                function cabin(choice) {
+                    if(choice ==1){
+                        document.getElementById("cabin").style.display = "none";
+                        document.getElementById("game").style.display = "block";
+                    
+                    }
+                    }
