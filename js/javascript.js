88619293da5dffc5957c12e6e16ac149b4ed5d99
Martin olsen <mbradyolsen@gmail.com>
2024-12-19 13:16:23 +0100

    new

--- js/javascript.js
+++ js/javascript.js
@@ -1,44 +1,42 @@
 function cabin(choice) {
-    if(choice ==1){
-       document.getElementById("p1").style.display = "none";
+    if (choice == 1) {
+        document.getElementById("p1").style.display = "none";
 
-       document.getElementById("cabin").style.display = "none";
+        document.getElementById("cabin").style.display = "none";
         document.getElementById("game").style.display = "block";
-    
-    }
-    }
-    function monster(choice) {
-        if(choice ==1){
-            document.getElementById("monster").style.display = "none";
-            document.getElementById("game").style.display = "block";
-        
-        }
-        }
-        
-        
-        function cabin(choice) {
-            if(choice ==1){
-                document.getElementById("cabin").style.display = "none";
-                document.getElementById("game").style.display = "block";
-            
-            }
-            }
 
-           
-           
-            function cabin(choice) {
-                if(choice ==1){
-                    document.getElementById("cabin").style.display = "none";
-                    document.getElementById("game").style.display = "block";
-                
-                }
-                }
-                
-                
-                function cabin(choice) {
-                    if(choice ==1){
-                        document.getElementById("cabin").style.display = "none";
-                        document.getElementById("game").style.display = "block";
-                    
-                    }
-                    }
+    }
+
+    if (choice == 2) {
+        document.getElementById("monster").style.display = "block";
+        document.getElementById("cabin").style.display = "none";
+        
+
+    }
+}
+
+
+function monster (choice) {
+    if (choice == 1) {
+        document.getElementById("monster").style.display = "none";
+        document.getElementById("game").style.display = "block";
+
+    }
+
+if (choice == 2) {
+    document.getElementById("monster").style.display = "none";
+    document.getElementById("game").style.display = "none";
+    document.getElementById("seng").style.display = "block";
+ 
+}
+}
+
+
+
+function seng() {
+    document.getElementById("monster").style.display = "none";
+    document.getElementById("game").style.display = "none";
+    document.getElementById("cabin").style.display = "block";
+    document.getElementById("p1").style.display = "none";
+    document.getElementById("seng").style.display = "none";
+}
